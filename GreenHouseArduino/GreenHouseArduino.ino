707c0d15cac42b872909c629ada75f1176247839
DariusKocar <darek.kocar@gmail.com>
2024-08-13 16:06:16 -0700

    Rucicka se otevira postupne

--- GreenHouseArduino/GreenHouseArduino.ino
+++ GreenHouseArduino/GreenHouseArduino.ino
@@ -3,10 +3,69 @@
 DHT11 dht11(D4);
 const int A1A = D3;
 const int A1B = D2;
-const int motorDelay = 30000; // ms
-const int openTemp = 26; // C
-const int closeTemp = 24; // C
+const int fullMotorTime = 20000; // ms
+const int openTemp = 30; // C
+const int closeTemp = 20; // C
 bool isOpen = false;
+int openPercentage = 0; // %
+int onePercentMS = 500; // %
+
+void open(int motorOnTime){
+        // Motor A
+        Serial.println("Motor OPEN start");
+        digitalWrite(A1A, HIGH);
+        digitalWrite(A1B, LOW);
+
+        delay(motorOnTime);
+
+        Serial.println("Motor OPEN stop");
+        digitalWrite(A1A, LOW);
+        digitalWrite(A1B, LOW);
+        isOpen = false;
+}
+
+void close(int motorOnTime){
+        // Motor A
+        Serial.println("Motor CLOSE start");
+        digitalWrite(A1A, LOW);
+        digitalWrite(A1B, HIGH);
+
+        delay(motorOnTime);
+
+        Serial.println("Motor CLOSE stop");
+        digitalWrite(A1A, LOW);
+        digitalWrite(A1B, LOW);
+        isOpen = true;
+}
+
+void fullClose(){
+  close(fullMotorTime);
+  openPercentage = 0;
+}
+
+void setOpenPercentage(int newPercentage){
+  if (newPercentage > openPercentage){
+    int diff = newPercentage - openPercentage;
+    open(diff * onePercentMS);
+    Serial.print("NEW percentage ");
+    Serial.print(newPercentage);
+    Serial.println("%");
+  }
+  if (newPercentage < openPercentage){
+    int diff = openPercentage - newPercentage;
+    close(diff * onePercentMS);
+    Serial.print("NEW percentage ");
+    Serial.print(newPercentage);
+    Serial.println("%");
+  }
+
+Serial.print("NEW percentage ");
+    Serial.print(newPercentage);
+    Serial.println("%");
+  openPercentage = newPercentage;
+}
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -17,36 +76,9 @@ void setup() {
   digitalWrite(A1A, LOW);
   digitalWrite(A1B, LOW);
 
-  close();
+  fullClose();
 }
 
-void open(){
-        // Motor A
-        Serial.println("Motor OPEN start");
-        digitalWrite(A1A, HIGH);
-        digitalWrite(A1B, LOW);
-
-        delay(motorDelay);
-
-        Serial.println("Motor OPEN stop");
-        digitalWrite(A1A, LOW);
-        digitalWrite(A1B, LOW);
-        isOpen = false;
-}
-
-void close(){
-        // Motor A
-        Serial.println("Motor CLOSE start");
-        digitalWrite(A1A, LOW);
-        digitalWrite(A1B, HIGH);
-
-        delay(motorDelay);
-
-        Serial.println("Motor CLOSE stop");
-        digitalWrite(A1A, LOW);
-        digitalWrite(A1B, LOW);
-        isOpen = true;
-}
 
 void loop() {
     int temperature = 0;
@@ -60,16 +92,22 @@ void loop() {
         Serial.print(humidity);
         Serial.println(" %");
 
-        if (isOpen){
-          if (temperature < closeTemp) {
-            close();
-          }
-        }
-        else {
-          if (temperature > openTemp) {
-            open();
-          }
-        }
+        Serial.print("Test: ");
+        Serial.println(((double)openTemp - closeTemp)/100.0);
+        Serial.print("Test2: ");
+        Serial.println(max(0, temperature - closeTemp));
+        int newPercentage = (int)100*(((double)openTemp - closeTemp)/100.0 * max(0, temperature - closeTemp));
+        setOpenPercentage(newPercentage);
+        // if (isOpen){
+        //   if (temperature < closeTemp) {
+        //     close();
+        //   }
+        // }
+        // else {
+        //   if (temperature > openTemp) {
+        //     open();
+        //   }
+        // }
     } else {
         // Print error message based on the error code.
         Serial.println(DHT11::getErrorString(result));
